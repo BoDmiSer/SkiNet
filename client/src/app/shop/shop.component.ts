2d9ba55129bf98b70fea8396c8d20289625d3a29
Дмитрий Богданов <bogdanov_s69@mail.ru>
2022-03-30 17:24:56 +0300

    Add Paginator and filters for Shop

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IBrand } from '../models/brand';
 import { IProduct } from '../models/product';
 import { IType } from '../models/productType';
+import { ShopParams } from '../models/shopParams';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,11 +11,20 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild('search',{static:true}) searchTerm:ElementRef
   products:IProduct[];
   brands:IBrand[];
   types:IType[];
   brandIdSelected = 0;
   typeIdSelected = 0;
+  sortSelected = 'name';
+  shopParams = new ShopParams();
+  totalCount: number;
+  sortOptions = [
+    {name: 'Alphabetical',value:'name'},
+    {name:'Price: Low to High', value:'priceAsc'},
+    {name:'Price: High to Low', value:'priceDesc'},
+  ]
 
   constructor(private shopService:ShopService) { }
 
@@ -25,8 +35,11 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts(this.brandIdSelected,this.typeIdSelected).subscribe(response =>{
+    this.shopService.getProducts(this.shopParams).subscribe(response =>{
       this.products = response.data;
+      this.shopParams.pageNumber = response.pageIndex;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
     },error=>{
       console.log(error);
     });
@@ -49,14 +62,39 @@ export class ShopComponent implements OnInit {
   }
   
   onBrandSelected(brandId: number){
-    this.brandIdSelected = brandId;
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number){
-    this.typeIdSelected = typeId;
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
+  onSortSelected(sort:string){
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+
+  onPageChanged(event:any){
+    if(this.shopParams.pageNumber !== event){
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch(){
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onReset(){
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
 
 }
